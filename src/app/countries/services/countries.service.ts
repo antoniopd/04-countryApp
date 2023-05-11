9d67e4eabc465267bc4fe180795668664473351b
antoniopd <antoniopdspain@gmail.com>
2023-05-11 12:07:11 +0200

    spinner y debounce

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, delay, map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
@@ -11,6 +11,15 @@ export class CountriesService {
 
 
   constructor(private http: HttpClient) { }
+
+  private getCountriesRequest( url: string): Observable<Country[]> {
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) ),
+        // delay( 2000 ),
+      );
+  }
+
 
   searchCountryByAlphaCode( code: string ): Observable<Country | null> {
 
@@ -28,10 +37,7 @@ export class CountriesService {
 
     const url = `${ this.apiUrl }/capital/${ term }`;
 
-    return this.http.get<Country[]>( url )
-      .pipe(
-      catchError( () => of([]) )
-      );
+    return this.getCountriesRequest(url);
   }
 
 
@@ -39,10 +45,7 @@ export class CountriesService {
 
     const url = `${ this.apiUrl }/name/${ term }`;
 
-    return this.http.get<Country[]>( url )
-      .pipe(
-      catchError( () => of([]) )
-      );
+    return this.getCountriesRequest(url);
 
   }
 
@@ -50,10 +53,7 @@ export class CountriesService {
 
     const url = `${ this.apiUrl }/region/${ region }`;
 
-    return this.http.get<Country[]>( url )
-      .pipe(
-      catchError( () => of([]) )
-      );
+    return this.getCountriesRequest(url);
 
   }
 
